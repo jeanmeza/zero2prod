00bfc084020faeb4a6bf509e66feae775de1b454
jeanmeza <meza.jean@outlook.com>
2024-01-21 21:46:51 +0100

    finished 4.5.9
    we set loggers to improve the observability posture of the application

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Zero to Production in Rust
 
 ## Packages to install
+
 - cargo install cargo-watch
 - cargo install cargo-tarpaulin
 - rustup component add clippy
@@ -9,4 +10,5 @@
 - cargo install --version="~0.7" sqlx-cli --no-default-features --features rustls,postgres
 - sudo apt-get install postgresql-client
 - config = "0.13"
-
+- cargo add tracing --features log
+  `tracing expands upon logging-style diagnostics by allowing libraries and applications to record structured events with additional information about temporary and causality - unlike a log message, a span in tracing has a beginning and end time, may be entered and exited by the flow of execution, and may exist within a nested tree of similar spans.`
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,16 +12,23 @@ pub struct FormData {
 
 pub async fn subscribe(_form: web::Form<FormData>, _pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        _form.email,
-        _form.name
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %_form.email,
+        subscriber_name = %_form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         insert into subscriptions (id, email, name, subscribed_at)
@@ -34,21 +42,15 @@ pub async fn subscribe(_form: web::Form<FormData>, _pool: web::Data<PgPool>) -> 
     // We use `get_ref` to get an immutable reference to the `PgPool`
     //  wrapped by `web::Data`.
     .execute(_pool.get_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            // Yes, this error log falls outside of `query_span`
+            // We'll rectify it later, pinky swear!
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
