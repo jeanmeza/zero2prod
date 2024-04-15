378da8c5e442c49c5f2d144c8e0821744aff81ae
jeanmeza <meza.jean@outlook.com>
2024-04-15 19:27:56 +0200

    finished 7.3.8.2 Testing hooks in our startup logic

--- src/startup.rs
+++ src/startup.rs
@@ -6,9 +6,54 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-use crate::configuration::Settings;
+use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    // We have converted the `build` function into a constructor for `Application`.
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
+
+        // We have removed the hard-coded `8000` - it's now coming from our settings!
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        // We "save" the bound port in one of `Application`'s fields
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    // A more expressive name that makes it clear that this function only returns when the
+    // application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
 
 pub fn run(
     listener: TcpListener,
@@ -33,25 +78,6 @@ pub fn run(
     Ok(server)
 }
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let lst = TcpListener::bind(address)?;
-    run(lst, connection_pool, email_client)
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
