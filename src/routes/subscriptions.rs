9c5355c4a93a2356f6264565f221d12e39305335
jeanmeza <meza.jean@outlook.com>
2024-04-16 22:18:08 +0200

    finished 7.7.2.2 A static confirmation link : Green test

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -46,14 +46,21 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     }
 
-    // Send an (useless) email to the new subscriber.
-    // We are ignoring email delivery errors for now.
+    let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
+
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
+            &format!(
+                "Welcome to our newsletter!<br>\
+                Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ),
+            &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
